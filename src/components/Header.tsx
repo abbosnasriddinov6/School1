d20101bd3666a754b0ca6d176572e5f1ef51db28
mirzayevt23 <mirzayevt23@gmail.com>
2024-06-03 00:48:59 +0500

    add functions

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,38 +1,35 @@
-import React from 'react'
+'use client'
+import React, {useState} from 'react'
 import '../scss/pages/Header.scss'
-import { bar, logo } from '@/assets'
+import {bar, logo} from '@/assets'
 import Image from 'next/image'
 import Link from 'next/link'
-import { RxHamburgerMenu } from "react-icons/rx";
-import { MdMenu } from "react-icons/md";
+import {RxHamburgerMenu} from "react-icons/rx";
+import {MdMenu} from "react-icons/md";
+import ResponsMenu from "@/components/ResponsMenu";
+
 const Header = () => {
+
+    const [showMenu, setShowMenu] = useState(false)
+
+    const SeeMenu = () => {
+        setShowMenu(true)
+        console.log('true buldi')
+    }
+
+
     return (
         <>
-            {/* <div className='head'>
-                <div className='container'>
-                    <div className='Header'>
-                        <div className='qale'>
-                            <div className='Logo'>
-                                <Link href='/' ><Image src={logo} alt="" /></Link>
-                            </div>
-                            <ul>
-                               
-                            </ul>
-                            <div className='buttons'>
-                                
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div> */}
-            <header className="head text-gray-600 body-font ">
+
+            <header className="head text-gray-600 body-font relative w-full ">
                 <div className=" container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
                     <div className="Header">
                         <div className="qale">
                             <div className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-                                <Link href='/' ><Image src={logo} alt="" /></Link>
+                                <Link href='/'><Image src={logo} alt=""/></Link>
                             </div>
-                            <nav className="ul md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
+                            <nav
+                                className="ul md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
                                 <Link href='/about'><p>Maktab haqida</p></Link>
                                 <Link href='/price'><p>Stipendiyalar</p></Link>
                                 <Link href='/qabuljarayon'><p>Qabul jarayoni</p></Link>
@@ -41,9 +38,14 @@ const Header = () => {
                             </nav>
                             <button className='button1'>+998 93 596 05 06</button>
                             <button className='button2'>Ru</button>
-                              
 
-                                <MdMenu className='text-black text-2xl block lg:hidden'/>
+                            {showMenu && <div
+                                className="absolute w-screen   top-0  right-[-400px]  ">
+                                <ResponsMenu showMenu={showMenu}/>
+                            </div>
+                            }
+
+                            <MdMenu onClick={() => SeeMenu()} className='text-black text-2xl block lg:hidden'/>
                         </div>
                     </div>
                 </div>
