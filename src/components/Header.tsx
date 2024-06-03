565dd6286509c5e5b3d9a1c941ca0d38863586d4
abbosnasriddinov6 <abbosbeknasriddinov001@gmail.com>
2024-06-03 14:16:27 +0500

    done

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,10 @@
 'use client'
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import '../scss/pages/Header.scss'
-import {bar, logo} from '@/assets'
+import { bar, logo } from '@/assets'
 import Image from 'next/image'
 import Link from 'next/link'
-import {RxHamburgerMenu} from "react-icons/rx";
-import {MdMenu} from "react-icons/md";
+import { MdMenu } from "react-icons/md";
 import ResponsMenu from "@/components/ResponsMenu";
 
 const Header = () => {
@@ -20,34 +19,18 @@ const Header = () => {
 
     return (
         <>
-<<<<<<< HEAD
-            {/* <div className='head'>
-                <div className='container'>
-                    <div className='Header'>
-                        <div className='qale'>
-                            <div className='Logo'>
-                                <Link href='/' ><Image src={logo} alt="" /></Link>
-                            </div>
-                            <ul>
-                               
-                            </ul>
-                            <div className='buttons'>
-                                
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div> */}
-            <header className="head text-gray-600 body-font  ">
-=======
+
+
+
+
 
             <header className="head text-gray-600 body-font relative w-full ">
->>>>>>> d20101bd3666a754b0ca6d176572e5f1ef51db28
+
                 <div className=" container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
                     <div className="Header">
                         <div className="qale">
                             <div className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-                                <Link href='/'><Image src={logo} alt=""/></Link>
+                                <Link href='/'><Image src={logo} alt="" /></Link>
                             </div>
                             <nav
                                 className="ul md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
@@ -62,11 +45,11 @@ const Header = () => {
 
                             {showMenu && <div
                                 className="absolute w-screen   top-0  right-[-400px]  ">
-                                <ResponsMenu showMenu={showMenu}/>
+                                <ResponsMenu showMenu={showMenu} />
                             </div>
                             }
 
-                            <MdMenu onClick={() => SeeMenu()} className='text-black text-2xl block lg:hidden'/>
+                            <MdMenu onClick={() => SeeMenu()} className='text-black text-2xl block lg:hidden' />
                         </div>
                     </div>
                 </div>
